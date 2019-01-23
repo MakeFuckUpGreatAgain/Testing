c0d03d0e7cdbf0c819cebfc64c4fc48af98a34fb
Bulgulator <Karol.Stepien@PCOSA.COM.PL>
2019-01-23 10:06:16 +0100

    Start ignore *.o

---
+++ include/Unit_Test.h
@@ -0,0 +1,17 @@
+#ifndef UNIT_TEST_H
+#define UNIT_TEST_H
+
+
+class Unit_Test
+{
+    public:
+        Unit_Test();
+        ~Unit_Test();
+        bool test;
+
+    protected:
+
+    private:
+};
+
+#endif // UNIT_TEST_H
---
+++ src/Unit_Test.cpp
@@ -0,0 +1,12 @@
+#include "Unit_Test.h"
+
+Unit_Test::Unit_Test()
+: test(false)
+{
+    //ctor
+}
+
+Unit_Test::~Unit_Test()
+{
+    //dtor
+}
